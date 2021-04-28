1473064e113e4e027a3775cb6dc3f64d0c077638
Abioye Abdullahi <walebioye@gmail.com>
2021-04-28 04:16:56 +0100

    tweaked app for heroku

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from whitenoise import WhiteNoise
-import os
+
 import django_heroku
 import dj_database_url
 from decouple import config
